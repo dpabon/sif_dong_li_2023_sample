af9499fa630348f5b5a3f62c9734fb3aede81502
dpabon <13040959+dpabon@users.noreply.github.com>
2025-12-03 10:05:39 +0100

    test

--- create_stac_catalog.py
+++ create_stac_catalog.py
@@ -1,3 +1,4 @@
 DATA_DIR = "./data"  # Directory with your GeoTIFF files
 OUTPUT_DIR = "./stac"  # Where STAC JSON files will be created
 GITHUB_REPO_URL = "https://github.com/yourusername/yourrepo"  # YOUR GitHub repo URL
+# minor chage
